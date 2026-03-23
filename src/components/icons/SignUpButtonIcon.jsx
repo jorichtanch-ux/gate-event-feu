48a7b79de7b26d05208c6f79b69892f60e771c90
jorichtanch-ux <r2023025971@feuroosevelt.edu.ph>
2026-03-23 14:16:41 +0800

    supabase

--- src/components/icons/SignUpButtonIcon.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { FaUserCheck } from "react-icons/fa";
-
-const SignUpButtonIcon = () => {
-  return <FaUserCheck {...props} />;
-}
-
-export default SignUpButtonIcon
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,50 +1,69 @@
 import Input from "../components/form/Input";
 import MainLayout from "../layouts/MainLayout";
 import Card from "../components/Card";
-import SignUpButtonIcon from "../components/icons/SignUpButtonIcon";
 import SignUpIcon from "../components/icons/SignUpIcon";
+import SendIcon from "../components/icons/SendIcon";
+import { supabase } from "../utils/supabase";
 
 const SignUp = () => {
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		const formData = new FormData(event.target);
+		const signupForm = {
+			firstname: formData.get("fistname"),
+			lastname: formData.get("lastname"),
+			email: formData.get("email"),
+			password: formData.get("password"),
+		};
+		const { data, error } = await supabase.auth.signUp({
+			email: signupForm.email,
+			password: signupForm.password,
+		});
+
+		if (error) alert(error);
+		if (data) console.log(data);
+	};
 	return (
 		<MainLayout>
 			<div className="min-h-screen flex flex-col">
 				<div className="flex justify-center items-center flex-1">
 					<Card>
 						<h1
-  className="text-xl"
-  style={{ fontFamily: "'Cedarville Cursive', cursive" }}
->
-  Sign Up
-</h1>
-						<Input
-							name="firstname"
-							placeholder="Enter your First Name"
-							label="Firstname"
-							type="text"
-						/>
-						<Input
-							name="lastname"
-							placeholder="Enter your Last Name"
-							label="Lastname"
-							type="text"
-						/>
-						<Input
-							name="email"
-							placeholder="Enter your Email"
-							label="Email"
-							type="email"
-						/>
-						<Input
-							name="password"
-							placeholder="Enter your Password"
-							label="Password"
-							type="password"
-						/>
-						<button className="btn btn-primary rounded-full mt-5"> 
-						 
-							Submit
-						</button>
-						<h1 classname="text-sm mt-4 text-center">Already have an account? <a href="/login"className="text-primary font-bold">Log in</a></h1>
+							className="text-xl"
+							style={{ fontFamily: "'Cedarville Cursive', cursive" }}
+						>
+							Sign Up
+						</h1>
+						<form onSubmit={handleSubmit}>
+							<Input
+								name="firstname"
+								placeholder="Enter your First Name"
+								label="Firstname"
+								type="text"
+							/>
+							<Input
+								name="lastname"
+								placeholder="Enter your Last Name"
+								label="Lastname"
+								type="text"
+							/>
+							<Input
+								name="email"
+								placeholder="Enter your Email"
+								label="Email"
+								type="email"
+							/>
+							<Input
+								name="password"
+								placeholder="Enter your Password"
+								label="Password"
+								type="password"
+							/>
+							<button className="btn btn-primary rounded-full mt-5">
+								<SendIcon className="text-sm" /> Submit
+							</button>
+						</form>
+						<h1 classname="text-sm mt-4 text-center">Already have an account? <a href="/login" className="text-primary font-bold">Log in</a></h1>
 					</Card>
 				</div>
 			</div>
