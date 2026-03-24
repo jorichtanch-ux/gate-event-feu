3a948254c0719fe043e05749370115168e107bbf
jorichtanch-ux <r2023025971@feuroosevelt.edu.ph>
2026-03-24 17:03:44 +0800

    editpage

--- src/pages/EditPage.jsx
+++ src/pages/EditPage.jsx
@@ -1,8 +1,77 @@
-import React from "react";
+import Input from "../components/form/Input";
 import MainLayout from "../layouts/MainLayout";
+import Card from "../components/Card";
+import SignUpIcon from "../components/icons/SignUpIcon";
+import SendIcon from "../components/icons/SendIcon";
+import { supabase } from "../utils/supabase";
+import { useContext } from "react";
+import { SessionContext } from "../contexts/SessionContext";
+import { useNavigate } from "react-router";
 
-const EditPage = () => {
-	return <MainLayout>This is the Edit Page</MainLayout>;
+const EditProfile = () => {
+	const session = useContext(SessionContext);
+	const navigate = useNavigate();
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		const formData = new FormData(event.target);
+		const signupForm = {
+			firstname: formData.get("firstname"),
+			lastname: formData.get("lastname"),
+			email: formData.get("email"),
+			password: formData.get("password"),
+		};
+
+		const { data: profileData, error: profileError } = await supabase
+			.from("profiles")
+			.update({
+				firstname: signupForm.firstname,
+				lastname: signupForm.lastname,
+				email: signupForm.email,
+			})
+			.eq("id", session.user.id)
+			.select();
+
+		if (profileError) alert(profileError);
+		if (profileData) {
+			navigate("/profile");
+		}
+	};
+
+	return (
+		<MainLayout>
+			<div className="min-h-screen flex flex-col">
+				<div className="flex justify-center items-center flex-1">
+					<Card>
+						<h1 className="text-xl font-bold">Edit Profile</h1>
+						<form onSubmit={handleSubmit}>
+							<Input
+								name="firstname"
+								placeholder="Enter your First Name"
+								label="Firstname"
+								type="text"
+							/>
+							<Input
+								name="lastname"
+								placeholder="Enter your Last Name"
+								label="Lastname"
+								type="text"
+							/>
+							<Input
+								name="email"
+								placeholder="Enter your Email"
+								label="Email"
+								type="email"
+							/>
+							<button className="btn btn-primary rounded-full mt-5">
+								<SendIcon className="text-sm" /> Submit
+							</button>
+						</form>
+					</Card>
+				</div>
+			</div>
+		</MainLayout>
+	);
 };
 
-export default EditPage;
+export default EditProfile;
