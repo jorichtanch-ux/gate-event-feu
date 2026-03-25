7705be55c0be7cce2084424bfae7667e62176461
jorichtanch-ux <r2023025971@feuroosevelt.edu.ph>
2026-03-25 15:05:23 +0800

    navigate event

--- src/pages/AddEvent.jsx
+++ src/pages/AddEvent.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import MainLayout from "../layouts/MainLayout";
 import Input from "../components/form/Input";
 import { supabase } from "../utils/supabase";
+import { useNavigate } from "react-router";
 
 const AddEvent = () => {
+	const navigate = useNavigate();
 	const handleSubmit = async (event) => {
 		event.preventDefault();
 		const formData = new FormData(event.target);
@@ -17,7 +19,7 @@ const AddEvent = () => {
 		.select()
 		.single()
 	if (eventError) alert(eventError);
-	if (eventData) console.log(eventData);
+	if (eventData) navigate("/manage-events");
 	};
 
 	return (
